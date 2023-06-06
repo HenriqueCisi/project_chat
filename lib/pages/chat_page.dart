bd3e6dfadb4a083547e4f56cffa035420cad6799
Henrique Cisi <henrique.cisi@accenture.com>
2023-06-05 22:21:07 -0300

    refactor chat widget

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:project_chat/components/messages.dart';
+import 'package:project_chat/components/new_message.dart';
 import 'package:project_chat/core/services/auth/auth_service.dart';
 
 class ChatPage extends StatelessWidget {
@@ -7,19 +9,34 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('Chat Page'),
-            TextButton(
-                onPressed: () {
-                  AuthService().logout();
-                },
-                child: const Text('Logout'))
+        appBar: AppBar(
+          title: const Text('ZIP ZAP'),
+          actions: [
+            DropdownButton(
+                icon: Icon(Icons.more_vert,
+                    color: Theme.of(context).primaryIconTheme.color),
+                items: [
+                  DropdownMenuItem(
+                      value: 'logout',
+                      child: Row(children: const [
+                        Icon(Icons.exit_to_app, color: Colors.black,),
+                        SizedBox(width: 10),
+                        Text('Logout')
+                      ])),
+                ],
+                onChanged: (value) {
+                  if (value == 'logout') {
+                    AuthService().logout();
+                  }
+                })
           ],
         ),
-      ),
-    );
+        body: SafeArea(
+            child: Column(
+          children: const [
+            Expanded(child: Messages()),
+            NewMessage(),
+          ],
+        )));
   }
 }
