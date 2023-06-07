0cf8075129b2878e47ebc720151fa3af9ac68300
Henrique Cisi <henrique.cisi@accenture.com>
2023-06-07 20:31:29 -0300

    adds chat + mock chat service

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,12 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:project_chat/core/models/chat_message.dart';
+import 'package:project_chat/core/services/chat/chat_service.dart';
 
 class Messages extends StatelessWidget {
   const Messages({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Messages'),
-    );
+    return StreamBuilder<List<ChatMessage>>(
+      stream: ChatService().messageStream(),
+      builder: (context, snapshot) {
+
+        if(snapshot.connectionState == ConnectionState.waiting){
+          return const Center(child: CircularProgressIndicator());
+        }
+        else if(!snapshot.hasData || snapshot.data!.isEmpty){
+          return const Center(child: Text('Start a conversation'));
+        }
+        else{
+          final msgs = snapshot.data!;
+
+          return ListView.builder(
+            reverse: true,
+            itemCount:  msgs.length,
+            itemBuilder: (ctx, index) {
+              return Text(msgs[index].text);
+            });
+        }
+
+        
+      },);
   }
 }
---
+++ lib/core/services/chat/chat_mock_service.dart
@@ -0,0 +1,60 @@
+import 'dart:async';
+import 'dart:math';
+
+import 'package:project_chat/core/models/chat_message.dart';
+import 'package:project_chat/core/models/chat_user.dart';
+import 'package:project_chat/core/services/chat/chat_service.dart';
+
+class ChatMockService implements ChatService {
+  static final List<ChatMessage> _msgs = [
+    ChatMessage(
+        id: '1',
+        text: 'Teste',
+        createdAt: DateTime.now(),
+        userId: '123',
+        userName: 'Teste',
+        userImageURL: 'assets/images/avatar.png'),
+    ChatMessage(
+        id: '2',
+        text: 'Teste2',
+        createdAt: DateTime.now(),
+        userId: '1234',
+        userName: 'Teste2',
+        userImageURL: 'assets/images/avatar.png'),
+    ChatMessage(
+        id: '2',
+        text: 'Teste3',
+        createdAt: DateTime.now(),
+        userId: '123',
+        userName: 'Teste3',
+        userImageURL: 'assets/images/avatar.png'),
+  ];
+
+  static MultiStreamController<List<ChatMessage>>? _controller;
+
+  static final _msgsStream = Stream<List<ChatMessage>>.multi((controller) {
+    _controller = controller;
+    controller.add(_msgs);
+  });
+
+  @override
+  Stream<List<ChatMessage>> messageStream() {
+    return _msgsStream;
+  }
+
+  Future<ChatMessage> save(String text, ChatUser user) async {
+    final newMessage = ChatMessage(
+        id: Random().nextDouble().toString(),
+        text: text,
+        createdAt: DateTime.now(),
+        userId: user.id,
+        userName: user.name,
+        userImageURL: user.imageURL);
+
+    _msgs.add(newMessage);
+
+    _controller?.add(_msgs);
+
+    return newMessage;
+  }
+}
