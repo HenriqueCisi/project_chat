032e5f5111308eebc5dcab7a7eb5b92c1d577739
Henrique Cisi <henrique.cisi@accenture.com>
2023-06-08 20:53:29 -0300

    add push notification service + provider dependency

---
+++ lib/core/services/notification/push_notification_service.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:project_chat/core/models/chat_notification.dart';
+
+class PushNotificationService with ChangeNotifier {
+  List<ChatNotification> _items = [];
+
+  List<ChatNotification> get items {
+    return [..._items];
+  }
+
+  void add(ChatNotification notification) {
+    _items.add(notification);
+    notifyListeners();
+  }
+
+  void remove(int i) {
+    _items.removeAt(i);
+    notifyListeners();
+  }
+}
